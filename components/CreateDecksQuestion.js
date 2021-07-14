3e05c32aba39a2d01e82c9b3f2c45f7ad08f4111
Tajudeen Abdulgafar <66276321+Abdulgafar4@users.noreply.github.com>
2021-07-14 09:45:51 +0100

    Completed and updated createDecksQuestion

--- components/CreateDecksQuestion.js
+++ components/CreateDecksQuestion.js
@@ -1,76 +1,78 @@
 import React, { Component } from 'react'
-import { View, Text, KeyboardAvoidingView, TouchableWithoutFeedback, TextInput } from 'react-native'
+import { View, Text, KeyboardAvoidingView, TouchableWithoutFeedback, TextInput, StyleSheet, Platform } from 'react-native'
 import { connect } from 'react-redux'
 import { createCard } from '../actions'
 import * as ApiDATA from '../utils/api'
 import SubmitButton from './SubmitButton'
 import allstyles from '../utils/style'
-import { red } from '../utils/colors'
+import { red, blue, gray } from '../utils/colors'
+import { FontAwesome } from '@expo/vector-icons';
+
 
 class CreateDecksQuestion extends Component {
   state = {
-    isQuestion: "",
-    isAnswer: "",
-    isError: false,
-    qError: false
+    quiz: "",
+    answer: "",
+    quizError: false,
   };
   createNewQuestion = () => {
-    const { isQuestion, isAnswer } = this.state
+    const { quiz, answer } = this.state
     const { createCard, navigation, title } = this.props   
-    if ( isQuestion && isAnswer ) {
-      ApiDATA.addCardToDeck(title, {isQuestion, isAnswer}) .then(() => {
-        createCard({title, card : {isQuestion, isAnswer} });
+    if ( quiz && answer ) {
+      ApiDATA.addCardToDeck(title, {quiz, answer}) .then(() => {
+        createCard({title, card : {quiz, answer} });
         this.setState({
-          isAnswer: "",
-          isQuestion: "",
-          isError: false,
-          qError : false
+          answer: "",
+          quiz: "",
         });
         navigation.goBack(); 
       })
     } else {
-      console.log(this.state.isError)
+      this.setState ({
+        quizError: quiz || answer ? false : true
+      })
     }
   };
 
   render() {
-       const { isError, qError } = this.state
        return (
-      <KeyboardAvoidingView style={allstyles.container} behavior="padding">
+      <KeyboardAvoidingView style={allstyles.container} behavior={Platform.OS === "ios" ? "padding" : "height"}>
         <TouchableWithoutFeedback>
           <View style={allstyles.NQdeckCard}>
             <Text style={allstyles.title}>Add a Question</Text>
             <TextInput
-              style={[
-                allstyles.textinput,
-                qError ? { borderColor: red } : ""
-              ]}
+              style={[ allstyles.textinput, this.state.quizError ? { borderColor: red } : "" ]}
               placeholder="Your Question..."
               value={this.state.title}
-              onSubmitEditing={() => this.refs.AnswerInput.focus()} //eslint-disable-line
+              onSubmitEditing={() => this.answerTextInput.focus()} //eslint-disable-line
               onChangeText={(text) =>
-                this.setState({ isQuestion: text, qError: false })}
+                this.setState({ quiz: text })}
               returnKeyType="next"
             />
             <TextInput
-              refs="AnswerInput"
+              ref="AnswerInput"
               style={[
-                allstyles.textinput,
-                isError ? { borderColor: red } : ""
+                allstyles.textinput, this.state.quizError ? { borderColor: red } : ""
               ]}
-              placeholder="The Answer..."
+              placeholder="Your Answer."
               value={this.state.title}
               onChangeText={(text) =>
-                this.setState({ isAnswer: text, isError: false })}
+                this.setState({ answer: text })}
               onSubmitEditing={this.createNewQuestion}
               returnKeyType="done"
             />
             <SubmitButton
-              style={[allstyles.button, allstyles.submitButton]}
+              style={[allstyles.button, allstyles.submitButton,
+               
+            ]}
               onPress={this.createNewQuestion}
             >
               Submit
             </SubmitButton>
+
+            <Text style={styles.notice}>
+             <FontAwesome name="info-circle" size={14} color={gray} /> The Answer Input Must Contain correct or wrong.
+           </Text>
           </View>
         </TouchableWithoutFeedback>
       </KeyboardAvoidingView>
@@ -79,6 +81,15 @@ class CreateDecksQuestion extends Component {
 
 
 }
+
+const styles = StyleSheet.create({
+  notice: {
+    textAlign: "center",
+    padding: 10,
+    fontSize: 14,
+    color: blue
+  }
+});
 
 function mapStateToProps (state, {navigation} ) {
   const { deckTitle} = navigation.state.params;
